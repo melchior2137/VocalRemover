012dba0d6807a7bce87aa9df6fbecd82ce9ed295
Mikołaj <84140494+melchior2137@users.noreply.github.com>
2023-03-07 21:59:10 +0100

    Delete VocalRemover.cmd

--- VocalRemover.cmd
+++
@@ -1,4 +0,0 @@
-call C:/Users/mikol/anaconda3/Scripts/activate.bat C:/Users/mikol/anaconda3
-call conda activate base
-python c:/Users/mikol/PythonProjects/youtube/main.py
-explorer.exe C:\Users\mikol\PythonProjects\youtube\audio
