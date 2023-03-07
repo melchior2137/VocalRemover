a313a8aab551050dbd616273f01fce151278c776
melchior2137 <84140494+melchior2137@users.noreply.github.com>
2023-03-07 22:06:53 +0100

    update gitignore

--- VocalRemover.cmd
+++ VocalRemover.cmd
@@ -1,4 +1,4 @@
 call C:/Users/mikol/anaconda3/Scripts/activate.bat C:/Users/mikol/anaconda3
 call conda activate base
-python c:/Users/mikol/PythonProjects/youtube/main.py
-explorer.exe C:\Users\mikol\PythonProjects\youtube\audio
+python c:/Users/mikol/PythonProjects/VocalRemover/main.py
+explorer.exe C:\Users\mikol\PythonProjects\VocalRemover\audio
